dc500d519cb9a9545c885d0db72d3479d10b4f71
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-03-29 23:44:55 -0500

    Update Account.h

--- Problemas2/Account.h
+++ Problemas2/Account.h
@@ -1,35 +1,48 @@
-#include <string>
+#include <iostream>
+#include "Account.h"
+using namespace std;
 
-class Account {
-public:
+int main()
+{
+    Account account1{ "Jane Green", 50 };
+    Account account2{ "John Blue", 78 };
+    int depositAmount, montoretiro;
 
-Account(std::string accountName, int initialBalance )
-: name{accountName} { 
-    if (initialBalance > 0) { 
-    balance = initialBalance;
-    }
 
+    cout << "account1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter deposit amount for account1: ";
+    cin >> depositAmount;
+    cout << "adding " << depositAmount << " to account1 balance";
+    account1.deposit(depositAmount);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter deposit amount for account2: ";
+    cin >> depositAmount;
+    cout << "adding " << depositAmount << " to account2 balance";
+    account2.deposit(depositAmount);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter withdraw amount for account1: ";
+    cin >> montoretiro;
+    cout << "adding " << montoretiro << " to account1 balance";
+    account1.retirar(montoretiro);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter withdraw amount for account2: ";
+    cin >> montoretiro;
+    cout << "adding " << montoretiro << " to account2 balance";
+    account2.retirar(montoretiro);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $"
+        << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $"
+        << account2.getBalance() << endl;
 }
-
-    void deposit(int depositAmount) {
-        if (depositAmount > 0) { 
-            balance = balance + depositAmount;
-        }
-    }
-    int getBalance() const {
-        return balance;
-}
-
- void setName(std::string accountName) {
- name = accountName;
- }
-
- 
- std::string getName() const {
- return name;
- }
- private:
-std::string name;
-int balance{0};
-
-};
