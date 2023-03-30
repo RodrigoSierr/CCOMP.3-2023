7c3fdb38502a6756783db63162fc6d0535c9f882
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-03-29 23:45:41 -0500

    Update AccountTest.cpp

--- Problemas2/AccountTest.cpp
+++ Problemas2/AccountTest.cpp
@@ -3,33 +3,46 @@
 using namespace std;
 
 int main()
- {
-    Account account1{"Jane Green", 50};
-    Account account2{"John Blue", -7};
+{
+    Account account1{ "Jane Green", 50 };
+    Account account2{ "John Blue", 78 };
+    int depositAmount, montoretiro;
 
- cout << "account1: " << account1.getName() << " balance is $"
- << account1.getBalance() ;
- cout << "\naccount2: " << account2.getName() << " balance is $"
- << account2.getBalance(); 
 
- cout << "\n\nEnter deposit amount for account1: "; 
- int depositAmount;
- cin >> depositAmount; 
- cout << "adding " << depositAmount << " to account1 balance";
-account1.deposit(depositAmount);
+    cout << "account1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
 
- cout << "\n\naccount1: " << account1.getName() << " balance is $"
- << account1.getBalance();
- cout << "\naccount2: " << account2.getName() << " balance is $"
- << account2.getBalance();
+    cout << "\n\nEnter deposit amount for account1: ";
+    cin >> depositAmount;
+    cout << "adding " << depositAmount << " to account1 balance";
+    account1.deposit(depositAmount);
 
- cout << "\n\nEnter deposit amount for account2: "; 
- cin >> depositAmount; 
- cout << "adding " << depositAmount << " to account2 balance";
-account2.deposit(depositAmount);
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
 
- cout << "\n\naccount1: " << account1.getName() << " balance is $"
- << account1.getBalance();
- cout << "\naccount2: " << account2.getName() << " balance is $"
- << account2.getBalance() << endl;
+    cout << "\n\nEnter deposit amount for account2: ";
+    cin >> depositAmount;
+    cout << "adding " << depositAmount << " to account2 balance";
+    account2.deposit(depositAmount);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter withdraw amount for account1: ";
+    cin >> montoretiro;
+    cout << "adding " << montoretiro << " to account1 balance";
+    account1.retirar(montoretiro);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter withdraw amount for account2: ";
+    cin >> montoretiro;
+    cout << "adding " << montoretiro << " to account2 balance";
+    account2.retirar(montoretiro);
+
+    cout << "\n\naccount1: " << account1.getName() << " balance is $"
+        << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $"
+        << account2.getBalance() << endl;
 }
